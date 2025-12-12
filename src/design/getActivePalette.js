557d1962d449f5c744598ffb7e79f6016e6e746d
ejlin2003 <ejlin2003@gmail.com>
2025-12-11 21:59:22 -0600

    Update angel story: add images, change status to draft, add Stephanie photo

--- src/design/getActivePalette.js
+++
@@ -1,8 +0,0 @@
-const { palettes } = require('./palettes');
-
-function getActivePalette() {
-  const key = process.env.PALETTE || process.env.NEXT_PUBLIC_PALETTE || 'current';
-  return palettes[key] || palettes.current;
-}
-
-module.exports = { getActivePalette };
